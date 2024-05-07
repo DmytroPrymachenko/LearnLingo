e407a219fffd0c59ca5150c9e95e0be73026a4f9
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-05-07 17:53:16 +0300

    фікс проміжний

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,35 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 import Home from "../pages/Home/Home";
 import Teachers from "../pages/Teachers/Teachers";
 import Favorites from "../pages/Favorites/Favorites";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "hooks/use-auth";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  useEffect(() => {
+    if (!user && location.pathname !== "/") {
+      dispatch();
+    }
+  }, [dispatch, user, location.pathname]);
+
   return (
     <>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/teachers" element={<Teachers />} />
-          <Route path="/favorites" element={<Favorites />} />
+          {user ? (
+            <>
+              <Route path="/teachers" element={<Teachers />} />
+              <Route path="/favorites" element={<Favorites />} />
+            </>
+          ) : (
+            <Route path="/" element={<Home />} />
+          )}
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
