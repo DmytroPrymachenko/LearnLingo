11b472418a85b15e9bf93e54d34079d74cccf547
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-05-21 17:04:26 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./store/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/learn-lingo/">
+  <BrowserRouter basename="/learn-lingo">
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
         <App />
